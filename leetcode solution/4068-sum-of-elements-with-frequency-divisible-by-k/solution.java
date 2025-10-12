88b9805e02e49e9cf913da431c0aa95e8fa269c5
Dev Kumar Singh <dineshsinghkumhrar@gmail.com>
2025-10-12 03:03:06 +0000

    [LeetCode Sync] Runtime - 3 ms (49.54%), Memory - 42.7 MB (54.45%)

---
+++ leetcode solution/4068-sum-of-elements-with-frequency-divisible-by-k/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int sumDivisibleByK(int[] nums, int k) {
+        int n = nums.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0 ; i<n ; i++){
+            map.put(nums[i] , map.getOrDefault(nums[i] , 0)+1);
+        }
+        int sum = 0;
+        
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            int num = entry.getKey();
+            int freq = entry.getValue();
+            if(freq % k == 0){
+                sum += (num*freq);
+            }
+        }
+        return sum;
+    }
+}
