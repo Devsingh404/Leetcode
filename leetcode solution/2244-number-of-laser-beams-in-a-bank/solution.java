4e7e1981ed72f7df4833181769464fcdb5f5ac0f
Dev Kumar Singh <dineshsinghkumhrar@gmail.com>
2025-10-27 17:01:35 +0000

    [LeetCode Sync] Runtime - 11 ms (77.73%), Memory - 45.2 MB (54.81%)

---
+++ leetcode solution/2244-number-of-laser-beams-in-a-bank/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfBeams(String[] bank) {
+        int prev = 0, result = 0;
+        for (String row : bank) {
+            int count = 0;
+            for (char c : row.toCharArray()) {
+                if (c == '1') count++;
+            }
+            if (count > 0) {
+                result += prev * count;
+                prev = count;
+            }
+        }
+        return result;
+    }
+}
