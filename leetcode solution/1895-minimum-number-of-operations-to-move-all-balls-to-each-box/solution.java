566161c3c0d18b62ccf4df5b7e231cf55f47dd4a
Dev Kumar Singh <dineshsinghkumhrar@gmail.com>
2025-11-16 16:01:02 +0000

    [LeetCode Sync] Runtime - 124 ms (41.85%), Memory - 46.4 MB (31.55%)

---
+++ leetcode solution/1895-minimum-number-of-operations-to-move-all-balls-to-each-box/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] minOperations(String boxes) {
+        int n = boxes.length();
+        int[] ans = new int[n];
+        for(int i=0 ; i<n ; i++){
+            int sum = 0;
+            for(int j=0 ; j<n ; j++){
+                if(j==i) continue;
+                if(boxes.charAt(j)=='1'){
+                    sum += Math.abs(j-i);
+                }
+            }
+            ans[i] = sum;
+        }
+        return ans;
+    }
+}
